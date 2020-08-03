cda654972ff31c7f9d1ba884c8080084af803500
Alexey Chechkovsky <alex.wihiss@gmail.com>
2020-08-03 10:25:01 -0400

    Build 0.1

---
+++ Time Fugitive/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    
+    public float speed;
+    public float jumpH;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+    }
+}
+
+
+
+
